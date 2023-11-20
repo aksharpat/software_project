bcf64be1b9fb766a361ef1a48f28d0dab5c1d88f
Jon Ginter <jonginter28@gmail.com>
2023-11-20 12:06:05 -0600

    Tickets are now displayed on page with images

--- src/BrowseTicketsPage.js
+++ src/BrowseTicketsPage.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 
 function BrowseTicketsPage() {
+    const lotteryTickets = [
+        {name: 'Power Ball', cost: '$2', image: 'PowerBall.png'},
+        {name: 'Mega Millions', cost: '$2', image: 'MegaMillions.png'},
+        {name: 'Lotto Texas', cost: '$1', image: 'TexasLottery.png'},
+        {name: 'Texas Two Step', cost: '$1.50', image: 'TexasTwoStep.png'}
+    ];
+
     return (
         <div className='browse-tickets-class'>
             <h1>Ticket Page</h1>
+            {lotteryTickets.map((ticket, index) =>(
+                <div key={index}>
+                    <img src={ticket.image} alt={ticket.name}/>
+                    <h2>{ticket.name}</h2>
+                    <p>Cost: {ticket.cost}</p>
+                    <p>Winnings: TBD</p>
+                </div>
+            ))}
             <Link to='/home'>
                 <button>Go Back</button>
             </Link>
