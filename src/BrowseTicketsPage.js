3bad780bebfd2b79cd6849417afbc31915f133cf
Jon Ginter <jonginter28@gmail.com>
2023-11-20 14:30:43 -0600

    You can now submit the numbers to local storage

--- src/BrowseTicketsPage.js
+++ src/BrowseTicketsPage.js
@@ -10,6 +10,29 @@ function BrowseTicketsPage() {
         {name: 'Texas Two Step', cost: '$1.50', image: 'TexasTwoStep.png'}
     ];
 
+    const [selectedTicket, setSelectedTicket] = useState(null);
+    const [numbers, setNumbers] = useState(Array(5).fill(''));
+    const [submittedNumbers, setSubmittedNumbers] = useState({});
+
+    const handleSelect = (index) => {
+        setSelectedTicket(index);
+    };
+
+    const handleNumberChange = (index, event) => {
+        const newNumbers = [...numbers];
+        newNumbers[index] = event.target.value;
+        setNumbers(newNumbers);
+    };
+
+    const handleSubmit = () => {
+        setSubmittedNumbers({
+            ...submittedNumbers, [lotteryTickets[selectedTicket].name]: numbers
+        });
+        setNumbers(Array(5).fill(''));
+
+        localStorage.setItem(lotteryTickets[selectedTicket].name, JSON.stringify(numbers));
+    }
+
     return (
         <div className='browse-tickets-class'>
             <h1>Ticket Page</h1>
@@ -19,6 +42,11 @@ function BrowseTicketsPage() {
                     <h2>{ticket.name}</h2>
                     <p>Cost: {ticket.cost}</p>
                     <p>Winnings: TBD</p>
+                    <button onClick={() => handleSelect(index)}>Select</button>
+                    {selectedTicket === index && numbers.map((number, i) =>(
+                        <input key={i} type="number" min="1" max="50" value={number} onChange={(event) => handleNumberChange(i, event)}/>
+                    ))}
+                    {selectedTicket === index && <button onClick={handleSubmit}>Submit</button> }
                 </div>
             ))}
             <Link to='/home'>
