0d60a097f6c366e3e9bfbabb483f5703a5a3e461
aksharpat <akshar0308@gmail.com>
2023-11-24 14:25:30 -0600

    order history

--- src/OrderHistory.js
+++ src/OrderHistory.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { getHistory } from './api';
 
-function OrderHistory() {
+const OrderHistory = () => {
+
+    const orderHistoryData = getHistory()
     return (
         <div className="history-page">
             <h1>Order History</h1>
+            {orderHistoryData.map((order, index) => (
+                <div key={index}>
+                    <h3>{order.Ticket}</h3>
+                    <p>Cost: {order.Cost}</p>
+                    <p>Winnings: {order.Winnings}</p>
+                    <p>Draw Date: {order.Draw_Date}</p>
+
+                    {order['Current?'] ? (
+                        <p>Your Numbers: {order.Your_Numbers}</p>
+                    ) : (
+                        <>
+                            {order.Your_Numbers === order.Winning_Numbers ? (
+                                <>
+                                    <p>You won!</p>
+                                    <p>Your Numbers: {order.Your_Numbers}</p>
+                                    <p>Winning Numbers: {order.Winning_Numbers}</p></>
+                            ) : (
+                                <>
+                                    <p>Your Numbers: {order.Your_Numbers}</p>
+                                    <p>Winning Numbers: {order.Winning_Numbers}</p>
+                                </>
+                            )}
+                        </>
+                    )}
+                    <hr />
+                </div>
+            ))}
         </div>
     );
-}
+};
 
 export default OrderHistory;
--- src/api.js
+++ src/api.js
@@ -1,12 +1,15 @@
 import data from './data/creds.json';
 import numbers from './data/report.json'
 import tickets from './data/tickets.json'
+import history from './data/history.json'
 
 export const getUsers = () => data;
 
 export const getNumbers = () => numbers;
 
 export const getTickets = () => tickets;
+
+export const getHistory = () => history;
 
 
 export const addUser = (newUser) => {
