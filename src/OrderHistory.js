49637c65d83bd74ff962cd5f78ababdd69bdcb6b
aksharpat <akshar0308@gmail.com>
2023-11-29 15:58:07 -0600

    buttons ayay

--- src/OrderHistory.js
+++ src/OrderHistory.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getHistory } from './api';
 
 const OrderHistory = () => {
 
     const orderHistoryData = getHistory()
+
+    const [showMessage, setShowMessage] = useState(false);
+    const [messageText, setMessageText] = useState('');
+
+    const handleDepositWinnings = (order) => {
+        // Handle deposit winnings logic
+        console.log(`Deposit winnings for order: ${order.Confirmation}`);
+    };
+
+    const handleSendEmailConfirmation = (order) => {
+        // Handle send email confirmation logic
+        console.log(`Send email confirmation for order: ${order.Confirmation}`);
+    };
+
+    const handleButtonClick = (message) => {
+        // Set the message to be displayed
+        setMessageText(message);
+
+        // Show the message when the button is clicked
+        setShowMessage(true);
+
+        // Hide the message after a delay (e.g., 3 seconds)
+        setTimeout(() => {
+            setShowMessage(false);
+        }, 3000);
+    };
+
     return (
         <div className="history-page">
             <h1>Order History</h1>
@@ -22,7 +49,8 @@ const OrderHistory = () => {
                                 <>
                                     <p>You won!</p>
                                     <p>Your Numbers: {order.Your_Numbers}</p>
-                                    <p>Winning Numbers: {order.Winning_Numbers}</p></>
+                                    <p>Winning Numbers: {order.Winning_Numbers}</p>
+                                </>
                             ) : (
                                 <>
                                     <p>Your Numbers: {order.Your_Numbers}</p>
@@ -31,7 +59,23 @@ const OrderHistory = () => {
                             )}
                         </>
                     )}
+                    {order.Winnings && order.Winnings.split('$')[1] < 599 && (
+                        <button onClick={() => handleButtonClick('Depositing Winnings...')}>
+                            Deposit Winnings
+                        </button>
+                    )}
+                    {order.Winnings && order.Winnings.split('$')[1] >= 599 && (
+                        <button onClick={() => handleButtonClick('Sending Email Confirmation...')}>
+                            Send Email Confirmation
+                        </button>
+                    )}
+                    {showMessage && (
+                        <div>
+                            {messageText}
+                        </div>
+                    )}
                     <hr />
+
                 </div>
             ))}
         </div>
